af735ff1ce7135f05d76febdff4426e21fa2ce78
Edward Huang <EdwardH@airbears2-10-142-71-136.airbears2.1918.berkeley.edu>
2016-02-16 12:50:18 -0800

    dimensions database checker in R

---
+++ db_checker.R
@@ -0,0 +1,8 @@
+## package to interface with web
+library(RCurl)
+
+## retrive sheets from google drive (requires sheet be published as a CSV)
+colEvent <- getURL('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?gid=0&single=true&output=csv')
+colEvent <- read.csv(textConnection(colEvent))
+head(colEvent)
+dim(colEvent)
