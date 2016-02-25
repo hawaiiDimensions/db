5d59b02ee6bce74973f252341875ece074d75e8f
Edward Huang <edwardghg@gmail.com>
2016-02-24 21:32:42 -0800

    converted dataframe factors to characters; retrieved invalid collector row indices

--- db_checker.R
+++ db_checker.R
@@ -1,8 +1,20 @@
 ## package to interface with web
 library(RCurl)
-
+## package to play around with dataframes
+install.packages("funModeling")
 ## retrive sheets from google drive (requires sheet be published as a CSV)
 colEvent <- getURL('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?gid=0&single=true&output=csv')
 colEvent <- read.csv(textConnection(colEvent))
-
+## convert to dataframe
+colEvent <- table(colEvent)
+## convert factors to characters
+colEvent[] <- lapply(colEvent, as.character)
+## diagnostic functions to keep in handy
+sapply(colEvent, class)
+str(colEvent)
 head(colEvent)
+db_status <- df_status(colEvent)
+## create dataframe of rows with empty values in "Collector"
+inval_col <- colEvent[,"Collector"] == ""
+## return indices of the rows of invalid collector columns
+which(inval_col, arr.ind = TRUE, useNames = TRUE)
