c0ff5e9856b0fa85564b84c29e340a4a791503c8
Edward Huang <edwardghg@gmail.com>
2016-03-08 21:46:10 -0800

    trivial

--- db_checker.R
+++ db_checker.R
@@ -16,41 +16,49 @@ sapply(colEvent, class)
 str(colEvent)
 head(colEvent)
 db_status <- df_status(colEvent)
+
 ## create dataframe of row with empty values in "Plot"
 ## return corrected indices of the rows of empty plot columns
 empt_plo <- colEvent[,"Plot"] == ""
 EMPTY_PLOT <- which(empt_plo, arr.ind = TRUE, useNames = TRUE) + 1
 EMPTY_PLOT
+
 ## create dataframe of row with empty values in "Date"
 ## return corrected indices of the rows of empty date columns
 empt_dat <- colEvent[,"Date"] == ""
 EMPTY_DATE <- which(empt_dat, arr.ind = TRUE, useNames = TRUE) + 1
 EMPTY_DATE
+
 ## create dataframe of rows with empty values in "Collector"; 
 ## return corrected indices of the rows of empty collector columns
 empt_col <- colEvent[,"Collector"] == ""
 EMPTY_COLLECTOR <- which(empt_col, arr.ind = TRUE, useNames = TRUE) + 1
 EMPTY_COLLECTOR
+
 ## create dataframe of rows with empty values in "Method";
 ## return corrected indices of the row of empty method columns
 empt_met <- colEvent[,"Method"] == ""
 EMPTY_METHOD <- which(empt_met, arr.ind = TRUE, useNames = TRUE) +1
 EMPTY_METHOD
+
 ## create dataframe of rows with empty values in "Whereabouts"; 
 ## return corrected indices of the rows of empty whereabouts columns
 empt_whe <- colEvent[,"Whereabouts"] == ""
 EMPTY_WHEREABOUTS <- which(empt_whe, arr.ind = TRUE, useNames = TRUE) + 1
 EMPTY_WHEREABOUTS
+
 ## create dataframe of rows with empty values in "SamplingRound"; 
 ## return corrected indices of the rows of empty smapling round columns
 empt_sam <- colEvent[,"SamplingRound"] == ""
 EMPTY_SAMPLINGROUND <- which(empt_sam, arr.ind = TRUE, useNames = TRUE) + 1
 EMPTY_SAMPLINGROUND
+
 ## create dataframe of rows with empty values in "NoOfVials"; 
 ## return corrected indices of the rows of empty vial number columns
 empt_via <- colEvent[,"NoOfVials"] == ""
 EMPTY_VIALNUM <- which(empt_via, arr.ind = TRUE, useNames = TRUE) + 1
 EMPTY_VIALNUM
+
 ## create dataframe of beating entries, create vector of 
 ## row indices of said entries
 beat_col <- colEvent[,"Method"] == "beating"
@@ -74,37 +82,31 @@ uni_beat_var <- unique(beat_var)
 ## indices with the beating rows indices and isolate the 
 ## duplicate values; adjust for accuracy
 empt_beat <- c(beat_ind, uni_beat_var)
-EMPTY_BEATING <- unique(empt_beat[duplicated(empt_beat)]) + 1
+EMPTY_BEATING <- sort(unique(empt_beat[duplicated(empt_beat)]) + 1, decreasing = FALSE)
 EMPTY_BEATING
+
 ## function to extract indices of empty row entries
 empty_indices <- function(column, dataframe) {
     empty_rows <- dataframe[,column] == ""
     indices <- which(empty_rows, arr.ind = TRUE, useNames = TRUE) + 1
     return(indices)
 }
+
 ## function to extract row indices of any empty entries in relevant
-## columns to the set of rows that have a certain entry in another
+## columns to the subset of rows that have a certain entry in another
 ## column; e.g. the row indices of empty entries of columns relevant
 ## to the method "beating".
-# empty_method <- function(column, method, dataframe, metavector) {
-#     method_col <- dataframe[,column] == method
-#     method_ind <- which(method_col, arr.ind = TRUE, useNames = TRUE)
-#     method_vec <- dataframe[,metavector] == ""
-#     vector_ind <- which(method_vec, arr.ind = TRUE, useNames = TRUE)
-#     unique_vec <- unique(vector_ind)
-#     empty_ind <- c(method_ind, unique_vec)
-#     empty_met <- unique(empty_ind[duplicated(empty_ind)]) + 1
-#     empty_met <- sort(empty_met, decreasing = FALSE)
-#     return(empty_met)
-# }
 empty_method <- function(column, method, dataframe, metavector) {
     method_col <- dataframe[,column] == method
     method_ind <- which(method_col, arr.ind = TRUE, useNames = TRUE)
-#     method_vec <- apply(dataframe[,metavector], 1, function(x) dataframe[,x] == "")
-#     vector_ind <- which(apply(dataframe[,metavector], 1, function(x) dataframe[,x] == ""), arr.ind = TRUE, useNames = TRUE)
-    unique_vec <- unique(which(apply(dataframe[,metavector], 1, function(x) c(dataframe[,x] == "")), arr.ind = TRUE, useNames = TRUE))
-    empty_met <- sort(unique(c(method_ind, unique(vector_ind))[duplicated(c(method_ind, unique(vector_ind)))]) + 1, decreasing = FALSE)
+    method_vec <- apply(colEvent[metavector], 2, function(x) which(x == "",arr.ind = TRUE, useName = TRUE))
+    unique_vec <- unique(unlist(method_vec, recursive = TRUE, use.names = FALSE))
+    empty_ind <- c(method_ind, unique_vec)
+    empty_met <- unique(empty_ind[duplicated(empty_ind)]) + 1
+    empty_met <- sort(empty_met, decreasing = FALSE)
     return(empty_met)
 }
-meta_beat <- c("Plant", "BeatingDuration", "TimeBegin", "TimeEnd")
-empty_method("Method", "beating", colEvent, meta_beat)
+
+metavector <- c("Plant", "BeatingDuration", "TimeBegin", "TimeEnd")
+empty_method("Method", "beating", colEvent, metavector)
+
