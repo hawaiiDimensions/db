f84f0a9481761331e1e80e34b9ba656529bcf909
ajrominger <ajrominger@gmail.com>
2016-09-02 10:45:41 -0700

    wrote script up to writing out checker file, now proceeding to summarizing number of events by site and method

--- dbCleaning_2016-09-02.R
+++ dbCleaning_2016-09-02.R
@@ -1,11 +1,21 @@
 setwd('~/Dropbox/hawaiiDimensions/db')
 source('db_checker.R', verbose = FALSE)
 
+## run diagnostics
 check <- DiagnoseDimensions(colEvent)
 
+## extract out needing info for checking
 colEventCheck <- colEvent
-colEventCheck$check <- ''
-check$empty$Plot
-colEvent$HDIM[check$duplicated]
-check$emptyMethod
+colEventCheck$check <- 'z'
+colEventCheck$check[colEventCheck$HDIM %in% check$empty$Plot] <- 'empty plot'
+colEventCheck$check[colEventCheck$HDIM %in% colEvent$HDIM[check$duplicated]] <- 'duplicated' 
+colEventCheck$check[colEventCheck$HDIM %in% check$emptyMethod] <- 'empty method'
 
+## order by what needs checking and write it out
+colEventCheck <- colEventCheck[order(colEventCheck$check, colEventCheck$HDIM), ]
+colEventCheck$check[colEventCheck$check == 'z'] <- NA
+write.csv(colEventCheck, file = 'dbCleaning_2016-09-02.csv', row.names = FALSE)
+
+
+## evaluate possible missing things on db that doesn't need checking
+colEventGood <- colEventCheck[is.na(colEventCheck$check), ]
