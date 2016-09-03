e9dcb57fa371581917fa4550d7b5397932877a06
ajrominger <ajrominger@gmail.com>
2016-09-02 17:44:22 -0700

    re-ran after more cleaning

--- dbCleaning_2016-09-02.R
+++ dbCleaning_2016-09-02.R
@@ -4,6 +4,11 @@ library(plyr)
 setwd('~/Dropbox/hawaiiDimensions/db')
 source('db_checker.R')
 
+colEvent$Date <- as.Date(colEvent$Date, '%m/%d/%Y')
+
+## round 2 starts June 1, 2016
+colEvent$SamplingRound <- ifelse(colEvent$Date >= as.Date('2015-06-01'), 2, 1)
+
 ## run diagnostics
 check <- DiagnoseDimensions(colEvent)
 
@@ -17,19 +22,14 @@ colEventCheck$check[colEventCheck$HDIM %in% check$emptyMethod] <- 'empty method 
 
 ## order by what needs checking and write it out
 colEventCheck <- colEventCheck[order(colEventCheck$check, colEventCheck$HDIM), ]
-colEventCheck$check[colEventCheck$check == 'z'] <- NA
+colEventCheck$check[colEventCheck$check == 'z'] <- ''
 write.csv(colEventCheck, file = 'dbCleaning_2016-09-02.csv', row.names = FALSE)
-
 
 ## =================================================================
 ## evaluate possible missing things on db that doesn't need checking
 ## =================================================================
 
-colEventGood <- colEventCheck[is.na(colEventCheck$check), ]
-colEventGood$Date <- as.Date(colEventGood$Date, '%m/%d/%Y')
-
-## round 2 starts June 1, 2016
-colEventGood$SamplingRound <- ifelse(colEventGood$Date >= as.Date('2015-06-01'), 2, 1)
+colEventGood <- colEventCheck[colEventCheck$check == '', ]
 
 ## get number of events by plot, method, round
 ceSum <- ddply(colEventGood[grep('beat|malaise|leaf|pit', colEventGood$Method), ], 
@@ -62,5 +62,4 @@ target[, 'pitfall'] <- 3
 ## missing
 ce1Mis <- ce1Sum - target
 ce2Mis <- ce2Sum - target
-
-
+ce2Mis
