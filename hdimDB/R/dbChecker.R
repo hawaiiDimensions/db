98bbdd150d0931ae26692a02a82e9e6ba4fcf41f
Edward Huang <edwardghg@gmail.com>
2016-09-14 13:54:25 -0700

    initial script

--- hdimDB/R/dbChecker.R
+++ hdimDB/R/dbChecker.R
@@ -1,19 +1,15 @@
 #' @title Checks Dimensions Database for errors
 #'  
-#' @description \code{makeLabels} runs through the online database and returns a list of HDIM numbers associated with specific errors
+#' @description \code{dbChecker} runs through the online database and returns a list of HDIM numbers associated with specific errors
 #' 
 #' @details See example
 #' 
-#' @param hdim the unique identifier(s) to be printed
-#' @param dir the directory in which to save the labels
-#' @param sheetName the file name to give the sheet of labels
-#' @param defaultYear the year to be given to labels with a missing year
-#' @param repID the number of labels for each unique identifier (can be a single value or a vector of length equal to length of \code{hdim})
+#' @param None
 #' 
 #' @return A multi-leveled list of HDIM numbers
 #' 
 #' @examples 
-#' dimensions
+#' dbChecker()
 #'
 #' @author Edward Greg Huang <edwardgh@@berkeley.edu>
 #' @export
