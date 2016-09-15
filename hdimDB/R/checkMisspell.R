659cfa19379dc9116dbe354440e5f1d87568324b
Edward Huang <edwardghg@gmail.com>
2016-09-14 23:54:52 -0700

    added script and tags

---
+++ hdimDB/R/checkMisspell.R
@@ -0,0 +1,64 @@
+#' @title Checks Dimensions Database for misspelled empty entries
+#'  
+#' @description \code{checkEmpty} runs through the online database and returns a list of HDIM numbers corresponding to misspelled entries in the database
+#' 
+#' @details Existing entries are compared against approved entries as listed in the synonym tables
+#' 
+#' @param None
+#' 
+#' @return List of character vectors of HDIM numbers
+#'
+#' @author Edward Greg Huang <edwardgh@@berkeley.edu>
+#' @export
+
+## Load the Database
+db <- readGoogle('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoew
+                    itaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
+db[is.na(db)] <- ""
+
+## Helper functions
+.misColumn <- function(column, vector){
+  indice.misspelled <- (which(!colEvent[, column] %in% vector))
+  return(colEvent[indice.misspelled,]$HDIM)
+}
+
+##################################################################
+## Vectors of Approved Entries - CORRECT WITH SYNONYM TABLE VALUES
+##################################################################
+
+## "Plot" (cor.plot) (IGNORE, MUST UPDATE FROM SYNONYM TABLES)
+cor.plot <- c(unique(colEvent$Plot), "")
+## "Collector" (cor.collect) (IGNORE, MUST UPDATE FROM SYNONYM TABLES)
+cor.collect <- c(unique(colEvent$Collector), "")
+## "Method" (cor.method)
+cor.method <- c("canopy malaise", "ground malaise", "beating", "pitfall", "canopy clipping", 
+                "leaf litter", "Insectazooka", "soil extraction", "")
+## "Plant" (cor.plant) (IGNORE, MUST UPDATE FROM SYNONYM TABLES)
+cor.plant <- c(unique(colEvent$Plant), "")
+## "BeatingDuration" (cor.beat)
+cor.beat <- c(0:300, "")
+## "PitFallSlice" (cor.pit)
+cor.pit <- c("up", "down", "A", "B", "C", "D", 
+             "E", "F", "ground", "")
+## "Whereabouts" (cor.where)
+cor.where <- c("UHH", "Hilgard 220", "NEED TO FIND", "")
+## "SamplingRound" (cor.sample)
+cor.sample <- c(1:2, "")
+## "NoOfVials" (cor.vial)
+cor.vial <- c(1:3, "")
+
+## List of the vectors of possible valid entries to columns being checked for misspellings
+cor.list <- list(cor.plot, cor.collect, cor.method, 
+                 cor.plant, cor.beat, cor.pit, 
+                 cor.where, cor.sample, cor.vial)
+
+## Vector of the different column names, excluding columns not being checked for misspellings
+misspelled.columns <- c("Plot", "Collector", "Method", "Plant", 
+                 "BeatingDuration", "PitFallSlice",
+                 "Whereabouts", "SamplingRound", "NoOfVials")
+
+checkEmpty <- function(){
+  misspell.list <- mapply(.misColumn, misspelled.columns, cor.list)
+  return(misspell.list)
+}
+
