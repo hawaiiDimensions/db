94b82a1b3c2ff2c4b7b50ab2c31f6980d3d7175a
Edward Huang <edwardghg@gmail.com>
2016-09-14 22:32:48 -0700

    duplicate hdim file

---
+++ hdimDB/R/duplicateHDIM.R
@@ -0,0 +1,16 @@
+#' @title Checks Dimensions Database for errors
+#'  
+#' @description \code{dbChecker} runs through the online database and returns a list of HDIM numbers associated with specific errors
+#' 
+#' @details See example
+#' 
+#' @param None
+#' 
+#' @return A multi-leveled list of HDIM numbers
+#' 
+#' @examples 
+#' dbChecker()
+#'
+#' @author Edward Greg Huang <edwardgh@@berkeley.edu>
+#' @export
+
