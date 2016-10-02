24eb6e0429f1dc1037000ac9a3413b3d3caec0f1
Edward Huang <edwardghg@gmail.com>
2016-10-01 23:35:48 -0700

    uploaded database and built ui sidepanel

--- dimensions_shiny/server.R
+++ dimensions_shiny/server.R
@@ -1,23 +1,18 @@
 ## dimensions_shiny server.R
 
+# devtools::install_github('hawaiiDimensions/db/hdimDB')
 library(shiny)
+library(datasets)
+library(hdimDB)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+function(input, output) {
+    db <- readGoogle('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
+    error.list <- dbChecker('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
     
-    # Expression that generates a histogram. The expression is
-    # wrapped in a call to renderPlot to indicate that:
-    #
-    #  1) It is "reactive" and therefore should be automatically
-    #     re-executed when inputs change
-    #  2) Its output type is a plot
     
-    output$distPlot <- renderPlot({
-        x    <- faithful[, 2]  # Old Faithful Geyser data
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'skyblue', border = 'white')
+    # Return the requested function(input, output) {
+    output$contents <- renderTable({
+        db
+        # errors <- db[rapply(), ] 
     })
-    
-})
+}
--- dimensions_shiny/ui.R
+++ dimensions_shiny/ui.R
@@ -1,24 +1,48 @@
-library(shiny)
+## dimensions_shiny ui.R
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-    
-    # Application title
-    titlePanel("Hello World!"),
-    
-    # Sidebar with a slider input for the number of bins
+fluidPage(
+    titlePanel("EvoLab Hawaii Dimensions Database Checker"),
+   
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 5,
-                        max = 50,
-                        value = 30)
-        ),
+            h6("University of California Berkeley - ESPM"),
+            h6("Version 0.0.1 - 1 October 2016"),
+            h6("Edward Greg Huang (edwardgh@berkeley.edu)"),
+            h3("Database Errors"),
+            tags$hr(),
+            checkboxInput('all.errors', label = 'All Errors', TRUE),
         
-        # Show a plot of the generated distribution
+            h4("Duplicates"),
+            checkboxInput("dup.hdim", label = "Duplicate HDIM Numbers", value = TRUE),
+        
+            checkboxGroupInput("missing.values", 
+                 label = h4("Missing Values"), 
+                 choices = list("Column Entries" = 1, 
+                                "Contingent Entries" = 2),
+                                   selected = NA),
+            
+            checkboxGroupInput("misspellings", 
+                 label = h4("Missppellings"), 
+                 choices = list("Column Entries" = 1), 
+                                #"Contingent Entries" = 2, ),
+                                   selected = NA),
+
+           checkboxGroupInput("invalid.time", 
+                 label = h4("Invalid Time Entries"), 
+                 choices = list("Date" = 1), 
+#                                "TimeBegin and TimeEnd" = 2,
+#                                "DateEnd" = 3, ),
+                                   selected = NA),
+
+            tags$hr(),
+            p('You can download the database as a .csv ',
+              a(href = 'https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv',
+                       'here.'))
+        ),
         mainPanel(
-            plotOutput("distPlot")
+            br(),
+            br(),
+            tableOutput('contents')
         )
     )
-))
+)
