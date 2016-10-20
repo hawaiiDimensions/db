ab04e2e8a8c4c384885a08797ddf0d0be45ce579
Edward Huang <edwardghg@gmail.com>
2016-10-20 13:59:29 -0700

    various changes

--- dimensions_shiny/server.R
+++ dimensions_shiny/server.R
@@ -1,18 +1,13 @@
 ## dimensions_shiny server.R
 
-# devtools::install_github('hawaiiDimensions/db/hdimDB')
 library(shiny)
-library(datasets)
-library(hdimDB)
+library(ggplot2)
 
 function(input, output) {
-    db <- readGoogle('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
-    error.list <- dbChecker('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
     
-    
-    # Return the requested function(input, output) {
-    output$contents <- renderTable({
-        db
-        # errors <- db[rapply(), ] 
+    # choose columns to display
+    db2 = db[sample(nrow(db), 1000), ]
+    output$mytable1 <- DT::renderDataTable({
+        DT::datatable(db2[, input$show_vars, drop = FALSE], options = list(orderClasses = TRUE))
     })
 }
--- dimensions_shiny/ui.R
+++ dimensions_shiny/ui.R
@@ -1,48 +1,26 @@
 ## dimensions_shiny ui.R
+library(shiny)
+library(ggplot2)
+library(hdimDB)
+db <- readGoogle('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
+errors <- dbChecker('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
+
 
 fluidPage(
-    titlePanel("EvoLab Hawaii Dimensions Database Checker"),
-   
+    title = 'Examples of DataTables',
     sidebarLayout(
         sidebarPanel(
-            h6("University of California Berkeley - ESPM"),
-            h6("Version 0.0.1 - 1 October 2016"),
-            h6("Edward Greg Huang (edwardgh@berkeley.edu)"),
-            h3("Database Errors"),
-            tags$hr(),
-            checkboxInput('all.errors', label = 'All Errors', TRUE),
-        
-            h4("Duplicates"),
-            checkboxInput("dup.hdim", label = "Duplicate HDIM Numbers", value = TRUE),
-        
-            checkboxGroupInput("missing.values", 
-                 label = h4("Missing Values"), 
-                 choices = list("Column Entries" = 1, 
-                                "Contingent Entries" = 2),
-                                   selected = NA),
-            
-            checkboxGroupInput("misspellings", 
-                 label = h4("Missppellings"), 
-                 choices = list("Column Entries" = 1), 
-                                #"Contingent Entries" = 2, ),
-                                   selected = NA),
-
-           checkboxGroupInput("invalid.time", 
-                 label = h4("Invalid Time Entries"), 
-                 choices = list("Date" = 1), 
-#                                "TimeBegin and TimeEnd" = 2,
-#                                "DateEnd" = 3, ),
-                                   selected = NA),
-
-            tags$hr(),
-            p('You can download the database as a .csv ',
-              a(href = 'https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv',
-                       'here.'))
+            conditionalPanel(
+                'input.dataset === "db"',
+                checkboxGroupInput('show_vars', 'Columns in diamonds to show:',
+                                   names(db), selected = names(db))
+            )
         ),
         mainPanel(
-            br(),
-            br(),
-            tableOutput('contents')
+            tabsetPanel(
+                id = 'dataset',
+                tabPanel('db', DT::dataTableOutput('mytable1'))
+            )
         )
     )
 )
