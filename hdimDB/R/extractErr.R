be3be8d28e3ea00acae97bb826ae235f640141ca
Edward Huang <edwardghg@gmail.com>
2016-11-18 16:14:49 -0800

    inital commit for assignCorr function

--- hdimDB/R/extractErr.R
+++ hdimDB/R/extractErr.R
@@ -1,5 +1,5 @@
 ## Assigns a given error type string to an HDIM number as a dataframe.
-extractErr <- function(errHDIM, errTag){
+.extractErr <- function(errHDIM, errTag){
     errHDIM <- unlist(errHDIM)
     if (is.null(names(errHDIM))){
         errMessage <- errTag
@@ -11,3 +11,19 @@ extractErr <- function(errHDIM, errTag){
     return(data.frame(errHDIM, errMessage))
 }
 
+.assignCorr <- function(errOut){
+    errTag <- gsub('\\..*', '', errOut$errMessage)[1]
+    if (errTag == "dupHDIM"){
+        corr <- NA
+    }
+    if (errTag == "empty"){
+        corr <- NA
+    }
+    if (errTag == "misspelled"){
+        corr <- 
+    }
+    if (errTag == "time"){
+        corr <- NA
+    }
+    return(data.frame(err.Out, corr))
+}
