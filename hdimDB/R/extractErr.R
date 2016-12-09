55ed2cbe01b4e0cdef21b2587c357ee4dd5c40f5
Edward Huang <edwardghg@gmail.com>
2016-12-09 15:40:54 -0800

    assignCorr implementation, added synVector switch statement and no error clause

--- hdimDB/R/extractErr.R
+++ hdimDB/R/extractErr.R
@@ -1,46 +1,58 @@
 ## Assigns a given error type string to an HDIM number as a dataframe.
 .extractErr <- function(db, errHDIM, errTag){ 
     errHDIM <- unlist(errHDIM)
-    if (is.null(names(errHDIM))){
-        errMessage <- errTag
+    if (length(errHDIM) == 0){
+        return(data.frame(errHDIM = NA, errMessage = NA, verbatim = NA))
     } else {
-        errType <- gsub('[[:digit:]]', '', names(errHDIM))
-        names(errHDIM) <- NULL
-        errMessage <- paste(errTag, errType, sep = ".")
+        if (is.null(names(errHDIM))){
+            errMessage <- errTag
+        } else {
+            errType <- gsub('[[:digit:]]', '', names(errHDIM))
+            names(errHDIM) <- NULL
+            errMessage <- paste(errTag, errType, sep = ".")
+        }
+        errColumn <- gsub('.*\\.', '', errMessage) 
+        verbatim <- try(mapply(hdim = errHDIM, col = errColumn, FUN = function(hdim, col){
+            paste(unique(db[db$HDIM == hdim, col]), collapse = ';')
+        }))
+        if(class(verbatim) == 'try-error') browser()
+        return(data.frame(errHDIM, errMessage, verbatim = verbatim))
     }
-    errColumn <- gsub('.*\\.', '', errMessage) 
-    verbatim <- mapply(hdim = errHDIM, col = errColumn, FUN = function(hdim, col){
-        paste(unique(db[db$HDIM == hdim, col]), collapse = ';')
-    })
-    return(data.frame(errHDIM, errMessage, verbatim = verbatim))
 }
 
 ## Input is output of .extractErr
 .assignCorr <- function(extractOut){
-    errTag <- gsub('\\..*', '', extractOut$errMessage)[1]
-    
-    if (errTag == "dupHDIM"){
+    if (any(is.na(extractOut$errHDIM))){
         corr <- NA
-    }
-    if (errTag == "empty"){
-        corr <- NA
-    }
-    if (errTag == "misspelled"){
-        errColumn <- gsub('.*\\.', '', extractOut$errMessage)
-        corr <- mapply(verbatim = extractOut$verbatim, column = errColumn, .closestMatch)
-    }
-    if (errTag == "time"){
-        corr <- NA
-    }
+    } else {
+        errTag <- gsub('\\..*', '', extractOut$errMessage)[1]
+        if (errTag == "dupHDIM"){
+            corr <- NA
+        }
+        if (errTag == "empty"){
+            corr <- NA
+        }
+        if (errTag == "misspelled"){
+            errColumn <- gsub('.*\\.', '', extractOut$errMessage)
+            corr <- mapply(verbatim = as.character(extractOut$verbatim), column = errColumn, .closestMatch)
+        }
+        if (errTag == "time"){
+            corr <- NA
+        }
+    } 
     return(data.frame(extractOut, corr))
 }
 
 .closestMatch <- function(verbatim, column){
-# .closestMatch <- function(verbatim, synVector){
-#     if(column == '...') { ## fill this in
-#         synVector <- NULL
-#     }
-    synVector <- switch(column)
+    synVector <- switch(column,
+                        'Plot' = .synValues(synPlotURL),
+                        'Collector' = .synValues(synCollectURL),
+                        'Method' = .synValues(synMethodURL),
+                        'Plant' = .synValues(synPlantURL),
+                        'PitFallSlice' = .synValues(synPitURL),
+                        'Whereabouts' = .synValues(synWhereURL),
+                        'SamplingRound' = c(1:2))
+    synVector <- synVector[synVector != '']
     distance <- RecordLinkage::levenshteinSim(verbatim, synVector)
     corr <- synVector[distance == max(distance)]
     if (length(corr) > 1){
