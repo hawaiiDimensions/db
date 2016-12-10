ad96ffa4f1988277885ffc88bfac422f2e5bb145
Edward Huang <edwardghg@gmail.com>
2016-12-10 13:07:56 -0800

    tidied things up.

--- hdimDB/R/checkMisspell.R
+++ hdimDB/R/checkMisspell.R
@@ -21,30 +21,20 @@
 checkMisspell <- function(db){
     db[is.na(db)] <- ""
     
-    ## Non-Synonym Correction Vectors ##
-
-    cor.sample <- c(1:2, "")
+    ## Non-Synonym Correction Vectors - 'cor.'
+    cor.sample <- c(1:2, "") # 'SamplingRound'
     
-    ## Synonym Sourced Correction Vectors ##
-    
-    # 'Plot
-    syn.plot <- .synValues(synPlotURL)
-    # 'Collector' 
-    syn.collect <- .synValues(synCollectURL)
-    # 'Method'
-    syn.method <- .synValues(synMethodURL)
-    # 'Plant'
-    syn.plant <- .synValues(synPlantURL)
-    # 'PitFallSlice'
-    syn.pit <- .synValues(synPitURL)
-    # 'Whereabouts'
-    syn.where <- .synValues(synWhereURL)
+    ## Synonym Sourced Correction Vectors - 'syn.'
+    syn.plot <- .synValues(synPlotURL) # 'Plot'
+    syn.collect <- .synValues(synCollectURL) # 'Collector'
+    syn.method <- .synValues(synMethodURL) # 'Method'
+    syn.plant <- .synValues(synPlantURL) # 'Plant'
+    syn.pit <- .synValues(synPitURL) # 'PitFallSlice'
+    syn.where <- .synValues(synWhereURL) # 'Whereabouts'
     
     # List of the vectors of possible valid entries to columns 
     # being checked for misspellings.
 
-    # 'syn-' synonym values.
-    # 'cor-' stand-in values.
     cor.list <- list(syn.plot, syn.collect, syn.method, 
                      syn.plant, syn.pit, syn.where, cor.sample)
     
@@ -59,11 +49,11 @@ checkMisspell <- function(db){
 }
 
 ## Hidden functions
-.misColumn <- function(column, vector, db){
+.misColumn <- function(column, vector, db){ 
     indice.misspelled <- (which(!db[, column] %in% vector))
-    return(db[indice.misspelled,]$HDIM)
+    return(db[indice.misspelled, ]$HDIM)
 }
 
-.synValues <- function(url){
+.synValues <- function(url){ 
     return(unique(readGoogle(url)[, 2]))
 }
