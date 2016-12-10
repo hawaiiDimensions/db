17093e2a926e8ee3300e96cb95e903cd1e0aac04
ajrominger <ajrominger@gmail.com>
2016-12-09 16:11:44 -0800

    updated roxygen tags

--- hdimDB/R/hdimURLS.R
+++ hdimDB/R/hdimURLS.R
@@ -1,24 +1,36 @@
 #' @title URLs for collection events and synonym tables
 #'  
-#' @description URLs for collection eventsand synonym tables
+#' @description URLs for collection events and synonym tables
 #' 
 #' @details URLs for collection events and synonym tables
 #' 
 #' @author Andy Rominger <ajrominger@@gmail.com>
 #' @export
+#' @rdname hdimURL
 
 colEventsURL <- 'https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv'
 
 # synonym tables
 #' @export
+#' @rdname hdimURL
 synPlotURL <- 'https://docs.google.com/spreadsheets/d/1Q8rFjF4n828ZVRTl7KkCQao5G0Emtwmm88MLZSoHcbA/pub?output=csv'
+
 #' @export
+#' @rdname hdimURL
 synCollectURL <- 'https://docs.google.com/spreadsheets/d/1_KGLPEcOneLvcRR8--CjEVeKTPDXJkI7YqwSTM2BVJc/pub?output=csv'
+
 #' @export
+#' @rdname hdimURL
 synMethodURL <- 'https://docs.google.com/spreadsheets/d/1MIXM5OzUtWUj4w_9dzf51Z1aRNV2mTCLUNgVBvkZYuE/pub?output=csv'
+
 #' @export
+#' @rdname hdimURL
 synPlantURL <- 'https://docs.google.com/spreadsheets/d/1SxbSt_SRiycfaihkOg-m4tz-nrtZNKtqJDE2OmG700Q/pub?output=csv'
+
 #' @export
+#' @rdname hdimURL
 synPitURL <- 'https://docs.google.com/spreadsheets/d/19eHQCQLKN_At10iUDmXYX2w1FA7EomHK9jZbq7lOSYg/pub?output=csv'
+
 #' @export
+#' @rdname hdimURL
 synWhereURL <- 'https://docs.google.com/spreadsheets/d/1sKJpNgcghZySIGQiw2o9t6Vt_Q06IVQo0GLf5YUb4-M/pub?output=csv'
