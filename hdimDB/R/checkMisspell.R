75b54fcaafa4808dbe94082f0bcb96bad58b393a
Edward Huang <edwardghg@gmail.com>
2016-12-20 23:33:45 -0800

    added empty entry redundancy prevention clause

--- hdimDB/R/checkMisspell.R
+++ hdimDB/R/checkMisspell.R
@@ -50,6 +50,8 @@ checkMisspell <- function(db){
 
 ## Hidden functions
 .misColumn <- function(column, vector, db){ 
+    # vector <- c(vector, '') # add empty string to synonym vector to avoid 
+    #                           redundantly tagging empty entries as misspellings
     indice.misspelled <- (which(!db[, column] %in% vector))
     return(db[indice.misspelled, ]$HDIM)
 }
