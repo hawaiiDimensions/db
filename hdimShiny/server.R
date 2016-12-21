3ed46d16405e948ca01a92a6339d77ad8abb3c9e
Edward Huang <edwardghg@gmail.com>
2016-12-21 14:21:30 -0800

    Shiny ColEvent app reinitialization. Basic db and checker display with search function.

--- hdimShiny/server.R
+++ hdimShiny/server.R
@@ -1,13 +1,37 @@
 ## dimensions_shiny server.R
-
 library(shiny)
-library(ggplot2)
+library(hdimDB)
+
+db <- readGoogle(colEventsURL)
+db2 = db[1:1000, ]
+errors <- dbChecker(db2)
 
 function(input, output) {
     
     # choose columns to display
-    db2 = db[sample(nrow(db), 1000), ]
     output$mytable1 <- DT::renderDataTable({
-        DT::datatable(db2[, input$show_vars, drop = FALSE], options = list(orderClasses = TRUE))
+        DT::datatable(db2)
+    })
+    output$mytable2 <- DT::renderDataTable({
+        DT::datatable(errors)
     })
 }
+
+# output$table <- DT::renderDataTable(DT::datatable({
+#     data <- db2
+#     if (input$Plot != "All") {
+#         data <- data[data$manufacturer == input$man,]
+#     }
+#     if (input$Date != "All") {
+#         data <- data[data$cyl == input$cyl,]
+#     }
+#     if (input$Colllector != "All") {
+#         data <- data[data$trans == input$trans,]
+#     }
+#     if (input$Colllector != "All") {
+#        data <- data[data$trans == input$trans,]
+#     }
+#     data
+# }))
+# 
+# }
--- hdimShiny/ui.R
+++ hdimShiny/ui.R
@@ -1,26 +1,36 @@
 ## dimensions_shiny ui.R
 library(shiny)
-library(ggplot2)
 library(hdimDB)
-db <- readGoogle('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
-errors <- dbChecker('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
-
 
 fluidPage(
-    title = 'Examples of DataTables',
-    sidebarLayout(
-        sidebarPanel(
-            conditionalPanel(
-                'input.dataset === "db"',
-                checkboxGroupInput('show_vars', 'Columns in diamonds to show:',
-                                   names(db), selected = names(db))
-            )
-        ),
+    title = 'HDIM Collection Events Database',
         mainPanel(
             tabsetPanel(
                 id = 'dataset',
-                tabPanel('db', DT::dataTableOutput('mytable1'))
+                tabPanel('db', DT::dataTableOutput('mytable1')),
+                tabPanel('errors', DT::dataTableOutput('mytable2'))
             )
         )
     )
-)
+
+
+# fluidPage(
+#     title = 'Examples of DataTables',
+#     sidebarLayout(
+#         sidebarPanel(
+#             conditionalPanel(
+#                 'input.dataset === "diamonds"',
+#                 checkboxGroupInput('show_vars', 'Columns in diamonds to show:',
+#                                    names(diamonds), selected = names(diamonds))
+#             )
+#         ),
+#         mainPanel(
+#             tabsetPanel(
+#                 id = 'dataset',
+#                 tabPanel('diamonds', DT::dataTableOutput('mytable1')),
+#                 tabPanel('mtcars', DT::dataTableOutput('mytable2')),
+#                 tabPanel('iris', DT::dataTableOutput('mytable3'))
+#             )
+#         )
+#     )
+# )
