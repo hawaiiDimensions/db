7ca798fd91c2d01db0a9ade8a743216103a0b799
Edward Huang <edwardghg@gmail.com>
2017-02-07 13:17:28 -0800

    renamed dbChecker to checkDb

--- hdimDB/R/checkDb.R
+++ hdimDB/R/checkDb.R
@@ -1,20 +1,20 @@
 #' @title Checks Dimensions Database for errors
 #'  
-#' @description \code{dbChecker} runs through the online database and returns a dataframe associated with specific errors
+#' @description \code{checkDb} runs through the online database and returns a dataframe associated with specific errors
 #' 
 #' @details Developed specifically for the Dimensions in Biodiversity Evolab Database.
 #' 
 #' @param db The database to be checked
 #' 
 # @example 
-# dbChecker('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
+# checkDb('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
 #' 
 #' @return Dataframe with HDIM identifier, error type, verbatim entry, and suggested correction.
 #'
 #' @author Edward Greg Huang <edwardgh@@berkeley.edu>
 #' @export
 
-dbChecker <- function(db){
+checkDb <- function(db){
     duplicatedHDIM <-dupHDIM(db)
     empty <- checkEmpty(db)
     misspelled <- checkMisspell(db)
