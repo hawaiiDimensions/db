2d7c42223b9d125123563bf988a73f969cbb40d1
Edward Huang <edwardghg@gmail.com>
2017-03-10 14:41:12 -0800

    downloadhandler implementation

--- hdimShiny/server.R
+++ hdimShiny/server.R
@@ -5,48 +5,25 @@ library(hdimDB)
 ## 
 db <- readGoogle(colEventsURL)
 db2 <- db[1:1000, ]
-errors <- checkDb(db2)
+# errors <- checkDb(db2)
+
 
 ## CURRENT VERSION ## 
 function(input, output) {
     
-    # choose columns to display
-    output$table1 <- DT::renderDataTable({
-        DT::datatable(db2)
-    })
-    output$table2 <- DT::renderDataTable({
-        DT::datatable(errors)
-    })
+    output$colEvents <- renderDataTable(readGoogle(colEventsURL))
+#     output$errors <- renderDataTable(checkDb(db2))
+    
+    output$downloadData <- downloadHandler(
+        filename = 'labels.csv',
+        content =  function(file) {
+            write.csv(readGoogle(colEventsURL), file)
+#         content = function(file) {
+#            makeLabels(hdim = as.list(strsplit(input$hdimList, ", ")[[1]]), 
+#                       dir = NULL, 
+#                       sheetName = file,
+#                       repID = input$repID)
+        }
+    )
 }
 
-## STAGED UPDATE ## 
-# function(input, output) {
-#     output$table <- DT::renderDataTable(DT::datatable({
-#         data <- db2
-#         if (input$HDIM != "All") {
-#             data <- data[data$HDIM == input$HDIM,]
-#         }
-#         if (input$Plot != "All") {
-#             data <- data[data$Plot == input$Plot,]
-#         }
-#         if (input$Date != "All") {
-#             data <- data[data$Date == input$Date,]
-#         }
-#         if (input$Colllector != "All") {
-#             data <- data[data$Collector == input$Collector,]
-#         }
-#         if (input$Method != "All") {
-#             data <- data[data$Method == input$Method,]
-#         }
-#         if (input$Whereabouts != "All") {
-#             data <- data[data$Whereabouts == input$Whereabouts,]
-#         }
-#         if (input$SamplingRound != "All") {
-#             data <- data[data$SamplingRound == input$SamplingRound,]
-#         }
-#         data
-#         }))
-##  output$table2 <- DT::renderDataTable({
-##      DT::datatable(errors)
-##      })
-#}
--- hdimShiny/ui.R
+++ hdimShiny/ui.R
@@ -1,18 +1,29 @@
 ## hawaii_dimensions_shiny ui.R
 library(shiny)
-library(hdimDB)
 
 ## CURRENT VERSION ## 
 fluidPage(
     titlePanel('Hawaii Dimensions Collection Events Database'),
+    p('Version 0.1 - UC Berkeley ESPM 2016'),
+    p('Developed by Edward G. Huang'),
+    sidebarLayout(
+        sidebarPanel(
+            h3('Label Sheet Maker'),
+            br(),
+            textInput(inputId = 'hdimList', label = 'HDIM Numbers', value = NA,
+                      placeholder = 'separate HDIMs with commas'),
+            
+            sliderInput(inputId = 'repID', label = 'Number of Labels per HDIM',
+                        value = 1, min = 1, max = 144),
+            downloadButton('downloadData', 'Download Label Sheet')
+        ),
         mainPanel(
-            p("Version 0.0 - UC Berkeley ESPM 2016"),
-            p("Developed by Edward G. Huang"),
             tabsetPanel(
                 id = 'dataset',
-                tabPanel('db', DT::dataTableOutput('table1')),
-                tabPanel('errors', DT::dataTableOutput('table2'))
+                tabPanel('db', dataTableOutput(outputId = 'colEvents')),
+                tabPanel('errors', dataTableOutput(outputId = 'errors'))
             )
         )
     )
+)
 
